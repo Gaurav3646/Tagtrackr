a5cc9f3dd23a209d119a917ee53b6a17fd69c81d
Gaurav <gauravverma3646@gamil.com>
2023-10-05 02:28:54 +0530

    login page ui changed.

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,11 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { GoogleButton } from "react-google-button";
 import { UserAuth } from "../../context/AuthContext";
 import { useNavigate } from "react-router-dom";
+import logo from "../assets/mac1.png";
 import "./login.scss";
+
 const Signin = () => {
   const { googleSignIn, currentUser } = UserAuth();
+  const [isHovered, setIsHovered] = useState(false);
   const navigate = useNavigate();
+
   const handleGoogleSignIn = async () => {
     try {
       await googleSignIn();
@@ -24,16 +28,30 @@ const Signin = () => {
 
   return (
     <div className="signInContainer">
-      <div className="modelcontainer">
-        <div className="model">
-          <div className="title">
-            <h1>Sign In</h1>
-          </div>
-          <div className="buttonContainer">
-            <span className="signInButton">
-              <GoogleButton onClick={handleGoogleSignIn} />
-            </span>
-          </div>
+      <div className="left">
+        <div className={`logo-container ${isHovered ? "hovered" : ""}`}>
+          <img
+            src={logo}
+            alt="Company Logo"
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+          />
+          <div className="logo-text">Admin Dashboard</div>
+        </div>
+      </div>
+
+      <div className="right">
+        <div className="intro">
+          <h1>Welcome to TagTracker</h1>
+          <p>
+            Track employee and student activities, attendance, and work hours
+            with ease.
+          </p>
+        </div>
+        <div className="buttonContainer">
+          <span className="signInButton">
+            <GoogleButton onClick={handleGoogleSignIn} />
+          </span>
         </div>
       </div>
     </div>
